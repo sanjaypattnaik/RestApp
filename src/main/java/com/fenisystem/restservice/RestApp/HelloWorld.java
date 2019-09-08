a438bc1e08b323c6d934234f33d33cad917650af
sanjaypattnaik <sanjay_pattnaik@yahoo.com>
2019-09-07 19:40:10 -0700

    Initil Version

---
+++ src/main/java/com/fenisystem/restservice/RestApp/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.fenisystem.restservice.RestApp;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping
+    public String index() {
+        return "Hello Feni System";
+
+    }
+}
